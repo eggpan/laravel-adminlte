22ce70c721d1c2b50fe3188ea3203f9fc13a696d
Eggpan <eggpan@eggpan.net>
2020-10-25 17:01:48 +0900

    Adminモデルにプロパティを追加

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Admin extends Authenticatable
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
