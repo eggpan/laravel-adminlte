a4156cd8dc4f0c1902386815e117ea7d249e8e91
Eggpan <eggpan@eggpan.net>
2020-10-27 22:31:33 +0900

    admin/viewにcard-headerがなかったので追加

--- resources/views/admin/admin/view.blade.php
+++ resources/views/admin/admin/view.blade.php
@@ -6,7 +6,10 @@
 
 @section('content')
 <div class="container">
-  <div class="card">
+  <div class="card card-primary">
+    <div class="card-header">
+      <h3 class="card-title">{{ __('lang.user_info') }}</h3>
+    </div>
     <div class="card-body">
       <table class="table table-bordered">
         <tr>
